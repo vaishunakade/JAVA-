5462a6d78ec120b1bebb28650abbd559651d0dd9
amitk <amitk@192.168.1.2>
2024-03-24 16:01:08 +0530

    Switch_Case

---
+++ Assignment/src/com/baisc/Switch_case_9.java
@@ -0,0 +1,24 @@
+package com.baisc;
+
+import java.util.Scanner;
+
+/*
+ WAP in java using switch case for following: Area of a circle Area of a square Area of a 
+ right angled triangle Area of a rectangle Circumference of a circle Perimeter of a square 
+ Accept inputs like radius,side,etc through keyboard.
+ */
+public class Switch_case_9 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the radius ");
+		float r = sc.nextFloat();
+		System.out.println("Enter the side ");
+		int s = sc.nextInt();
+		
+		
+		
+
+	}
+
+}
